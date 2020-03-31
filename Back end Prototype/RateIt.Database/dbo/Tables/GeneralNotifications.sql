6136180c721b4d42366621a5f9b234156b359015
Matt <matt19sharp@gmail.com>
2020-03-31 13:32:18 +0800

    Commit before credit wallet change

---
+++ Back end Prototype/RateIt.Database/dbo/Tables/GeneralNotifications.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[GeneralNotifications]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [Text] NVARCHAR(MAX) NOT NULL, 
+    [Type] NVARCHAR(100) NOT NULL, 
+    [Timestamp] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [UserId] BIGINT NOT NULL, 
+    [Display] BIT NOT NULL DEFAULT 1, 
+    [SystemData] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_GeneralNotifications_ToUsers] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
+)
